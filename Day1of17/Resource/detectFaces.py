fe8293dd94467f90eab99eb2a5546e46e939bc98
DucTrungNguyen98 <ductrungnguyen98@gmail.com>
2020-04-07 23:31:39 +0700

    day1

--- Day1of17/Resource/detectFaces.py
+++ Day1of17/Resource/detectFaces.py
@@ -4,7 +4,52 @@ import cv2
 
 def main():
     ap = argparse.ArgumentParser()
+    ap.add_argument("-i", "--image", required= True, help="path to input image")
+    ap.add_argument("-p","--prototxt",required=True, help="path to caffe deloy prototxt")
+    ap.add_argument("-m", "--model", required= True, help="path to caffe model")
+    ap.add_argument("-c", "---confidence", type= float, default=0.7,
+    help="minium probality to filter weak detections")
+
+    print('ass')
+
+    args = vars(ap.parse_args())
 
 
-if __name__ == '_main_':
+    print('loading model...')
+    net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
+
+    # load input image
+    image = cv2.imread(args['image'])
+    (h,w) = image.shape[:2] 
+    blob = cv2.dnn.blobFromImage(cv2.resize(image, (300,300)), 1.0,
+        (300,300), (104.0,77.0,123.0))
+   
+
+    print('computin object detections...')
+    net.setInput(blob)
+    detections = net.forward()
+
+    for i in range(0, detections.shape[2]):
+        confidence = detections[0,0,i,2]
+        # print(detections)
+
+        if confidence > args["confidence"]:
+            box = detections[0,0,i, 3:7] * np.array([w,h,w,h])
+            (startX, startY, endX, endY) = box.astype("int")
+
+            text = "{:.2f}%".format(confidence *100)
+            y = startY - 10 if startY - 10 > 10 else startY + 10
+            cv2.rectangle(image, (startX, startY), (endX, endY), (0,0,255), 2)
+            cv2.putText(image, text,  (startX, y),
+                cv2.FONT_HERSHEY_COMPLEX, 0.45, (0,0,255), 2)
+    
+    cv2.imshow("Output", image)
+    cv2.waitKey(0)
+
+
+
+
+
+
+if __name__ == '__main__':
     main()
